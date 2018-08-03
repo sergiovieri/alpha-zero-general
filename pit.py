ebb178a36fad376705cbdfb572511af432490311
Sergio Vieri <sergio.vieri.hp@gmail.com>
2018-08-03 17:43:45 +0700

    Change drawThreshold of pit to 200

--- pit.py
+++ pit.py
@@ -18,7 +18,7 @@ def argmax(self, pi):
   return np.random.choice(np.flatnonzero(pi == mx))
 
 g = TroopsGame(8, dotdict({
-    	'drawThreshold': 100
+    	'drawThreshold': 200
 		}))
 
 # all players
@@ -29,14 +29,14 @@ g = TroopsGame(8, dotdict({
 # nnet players
 n1 = NNet(g)
 n1.load_checkpoint('./temp/','best.pth.tar')
-args1 = dotdict({'numMCTSSims': 50, 'cpuct':2.0})
+args1 = dotdict({'numMCTSSims': 500, 'cpuct':2.0})
 mcts1 = MCTS(g, n1, args1)
 n1p = lambda x: argmax(mcts1.getActionProb(x, temp=1))
 
 
 n2 = NNet(g)
 n2.load_checkpoint('./temp/','best.pth.tar')
-args2 = dotdict({'numMCTSSims': 50, 'cpuct':2.0})
+args2 = dotdict({'numMCTSSims': 500, 'cpuct':2.0})
 mcts2 = MCTS(g, n2, args2)
 n2p = lambda x: argmax(mcts2.getActionProb(x, temp=1))
 
